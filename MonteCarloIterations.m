4a8f5a9e7b23a88d914fe1bcc5d49ae59ee686c2
hugopereira-eng <hugo.c.pereira@tecnico.ulisboa.pt>
2023-02-01 12:21:32 +0000

    Implemented steepest descent

--- MonteCarloIterations.m
+++ MonteCarloIterations.m
@@ -10,18 +10,16 @@ addpath("funcs\")
 h0 = 1;                 % Angular momentum of each CMG
 beta = 54.73*pi/180;    % Pyramid skew angle
 
-%% Gradient
+%% Gradient & Hessian
 g = sym('g',[4 1]);
 J = PyramidJacobian(g,h0,beta);
 eq = det(J*J');
-Dg1 = diff(eq,g(1));
-Dg2 = diff(eq,g(2));
-Dg3 = diff(eq,g(3));
-Dg4 = diff(eq,g(4));
+Dg = gradient(eq);
+Hg = hessian(eq);
 
 %% Random set
 % Uniformly generated samples
-samples = 1000;
+samples = 10;
 G = zeros(4,samples);
 ind = 0;
 for i = 1:samples
@@ -32,28 +30,38 @@ for i = 1:samples
 end
 
 %% Gradient descent
-alpha = 0.1;         % learning rate
-sigma = 0.091;       % Standard deviation (perturbations)
+sigma = 0.001;       % Standard deviation (perturbations)
 I = zeros(1,samples);
 hbar = waitbar(0,'Simulation Progress');
 for i = 1:length(G)
     g1 = G(1,i); g2 = G(2,i); g3 = G(3,i); g4 = G(4,i);
-    J = PyramidJacobian([g1 g2 g3 g4],h0,beta);
+    g = [g1;g2;g3;g4];
+    J = PyramidJacobian(g,h0,beta);
     D = det(J*J');
     iterations = 0;
     while D(iterations+1) > 10e-3
+
+        % alpha
+        Dv = double(subs(Dg));
+        Hv = double(subs(Hg));
+        gradD = Dv - Hv*g;
+        alpha = gradD'*gradD/(gradD'*Hv*gradD);
+
         % Update
-        t1 = g1 - alpha*double(subs(Dg1));
-        t2 = g2 - alpha*double(subs(Dg2));
-        t3 = g3 - alpha*double(subs(Dg3));
-        t4 = g4 - alpha*double(subs(Dg4));
-        % Add perturbation - generated from a Gaussian distribution
-        g1 = t1 + normrnd(0,sigma); 
-        g2 = t2 + normrnd(0,sigma);
-        g3 = t3 + normrnd(0,sigma); 
-        g4 = t4 + normrnd(0,sigma);
+        g = g - alpha*gradD;
+
+%         % Update
+%         t1 = g1 - alpha*double(subs(Dg1));
+%         t2 = g2 - alpha*double(subs(Dg2));
+%         t3 = g3 - alpha*double(subs(Dg3));
+%         t4 = g4 - alpha*double(subs(Dg4));
+%         % Add perturbation - generated from a Gaussian distribution
+%         g1 = t1 + normrnd(0,sigma); 
+%         g2 = t2 + normrnd(0,sigma);
+%         g3 = t3 + normrnd(0,sigma); 
+%         g4 = t4 + normrnd(0,sigma);
         
-        J = PyramidJacobian([g1 g2 g3 g4],h0,beta);
+        J = PyramidJacobian(g,h0,beta);
         D = [D det(J*J')];
 
         iterations = iterations + 1;
